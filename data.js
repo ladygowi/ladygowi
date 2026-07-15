a9802687c8fac984797db45bbb50ffdc48bb44be
ladygowi <lasectagow@gmail.com>
2026-07-14 21:42:09 -0400

    Elimina sonido: Campanita1

--- data.js
+++ data.js
@@ -46,7 +46,6 @@ const CANCIONES = [
    ============================ */
 const SONIDOS = [
   { tipo:"campana", nombre:"Campanita", archivo:"sonidos/1784079723458-campanita1.mp3", peso:1 },
-  { tipo:"campana", nombre:"Campanita1", archivo:"sonidos/1783110064354-campanita1.mp3", peso:1 },
   { tipo:"chibi", nombre:"Oveja seria", archivo:"sonidos/1783110049122-ovejita-seria.mp3", peso:1 },
   { tipo:"chibi", nombre:"Oveja chillona", archivo:"sonidos/1783110036092-ovejita-chillona.mp3", peso:1 },
   { tipo:"chibi", nombre:"Oveja chikita", archivo:"sonidos/1783110024397-ovejita-chikita.mp3", peso:1 },
